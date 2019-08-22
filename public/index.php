19d106414100d6a88cdeabfc7a0082aab2828ef7
NikitaMakovey <niks.finn@gmail.com>
2019-08-23 04:27:03 +1000

    #2 Add some changes with regular expressions

--- public/index.php
+++ public/index.php
@@ -1,19 +1,24 @@
 <?php
 
 //echo $query = rtrim(substr($_SERVER['QUERY_STRING'], 10), "/");
-echo $query = rtrim($_SERVER['REQUEST_URI'], "/");
+echo $query = rtrim($_SERVER['REQUEST_URI'], '/');
 
 require '../vendor/core/Router.php';
 require '../vendor/libs/functions.php';
 
-Router::add('posts/add', ['controller' => 'Posts', 'action' => 'add']);
-Router::add('posts', ['controller' => 'Posts', 'action' => 'index']);
-Router::add('', ['controller' => 'Main', 'action' => 'index']);
+//Router::add('posts/add', ['controller' => 'Posts', 'action' => 'add']);
+//Router::add('posts', ['controller' => 'Posts', 'action' => 'index']);
+//Router::add('', ['controller' => 'Main', 'action' => 'index']);
+
+Router::add('^?', ['controller' => 'Main', 'action' => 'index']);
+Router::add('(?P<controller>[a-z-]+)/?(?P<action>[a-z-]+)?');
 
 debug(Router::getRoutes());
 
-if (Router::matchRoute($query)) {
-    debug(Router::getRoute());
-} else {
-    echo '404';
-}
+Router::dispatch($query);
+
+//if (Router::matchRoute($query)) {
+//    debug(Router::getRoute());
+//} else {
+//    echo '404';
+//}
--- vendor/core/Router.php
+++ vendor/core/Router.php
@@ -3,11 +3,6 @@
 
 class Router
 {
-//    public function __construct()
-//    {
-//        echo "Router";
-//    }
-
     protected static $routes = [];
     protected static $route = [];
 
@@ -25,11 +20,21 @@ class Router
 
     public static function matchRoute($url) {
         foreach (self::$routes as $pattern => $route) {
-            if ($url == $pattern) {
+            if (preg_match("#$pattern#i", $url, $matches)) {
+                debug($matches);
                 self::$route[] = $route;
                 return true;
             }
         }
         return false;
+    }
+
+    public static function dispatch($url) {
+        if (self::matchRoute($url)) {
+
+        } else {
+            http_response_code(404);
+            include '../../public/404.html';
+        }
     }
 }
