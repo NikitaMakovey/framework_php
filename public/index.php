fd823d59cac5b2b1a81d8b7256572c8d277cd534
NikitaMakovey <niks.finn@gmail.com>
2019-08-23 02:27:42 +1000

    #2 Create Class Router and make the base part

--- public/index.php
+++ public/index.php
@@ -1,3 +1,19 @@
 <?php
 
-echo $_REQUEST;
+//echo $query = rtrim(substr($_SERVER['QUERY_STRING'], 10), "/");
+echo $query = rtrim($_SERVER['REQUEST_URI'], "/");
+
+require '../vendor/core/Router.php';
+require '../vendor/libs/functions.php';
+
+Router::add('posts/add', ['controller' => 'Posts', 'action' => 'add']);
+Router::add('posts', ['controller' => 'Posts', 'action' => 'index']);
+Router::add('', ['controller' => 'Main', 'action' => 'index']);
+
+debug(Router::getRoutes());
+
+if (Router::matchRoute($query)) {
+    debug(Router::getRoute());
+} else {
+    echo '404';
+}
