3c339eb04d73204e035d9b0cfc79a004d7a8143d
NikitaMakovey <niks.finn@gmail.com>
2019-08-23 20:08:35 +1000

    #1 Autoload include for controllers

--- public/index.php
+++ public/index.php
@@ -3,11 +3,17 @@
 $query = rtrim($_SERVER['REQUEST_URI'], '/');
 $query = strlen($query) > 1 && $query[0] == '/' ? substr($query, 1) : $query;
 
+define('APP', dirname(__DIR__) . '/app');
+
 require '../vendor/core/Router.php';
 require '../vendor/libs/functions.php';
-require '../app/controllers/Main.php';
-require '../app/controllers/Posts.php';
-require '../app/controllers/PostsNew.php';
+
+spl_autoload_register(function ($class) {
+    $file = APP . "/controllers/$class.php";
+    if (file_exists($file)) {
+        require_once $file;
+    }
+});
 
 Router::add('^$', ['controller' => 'Main', 'action' => 'index']);
 Router::add('^(?P<controller>[a-z-]+)/?(?P<action>[a-z-]+)?$');
